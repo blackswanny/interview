07d167c6b30d849a3764fcaa3101b525d179b462
RT <rt@nyzhnyk.name>
2015-09-26 12:45:21 -0700

    Metadata

--- config/env/default.js
+++ config/env/default.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    title: 'Interview',
+    description: 'WebRTC (OpenTok) demo',
+    keywords: 'WebRTC, OpenTok',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
   port: process.env.PORT || 3000,
